beace98888caaf90a98aec92df8daffa0228b9f9
LOHITH MR <116547274+LOHITH494@users.noreply.github.com>
2023-11-21 21:40:27 +0530

    Create Write a function.py

---
+++ Write a function.py
@@ -0,0 +1,11 @@
+#Write a function
+def is_leap(year):
+    leap = False
+    if(year%100==0):
+            if(year % 400==0):
+                    leap=True
+    elif(year%4==0):
+            leap=True
+    else:
+            leap=False
+    return leap
